017ca402fff0e9ad7e96c236ec7b0ccdbf068677
NanoCode012 <kevinvong@rocketmail.com>
2017-10-23 14:53:22 +0700

    Added new repo on C#
    Following a course on Udemy by Mosh Hamedani.

---
+++ CSharpIntermediate_Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpIntermediate_Classes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
