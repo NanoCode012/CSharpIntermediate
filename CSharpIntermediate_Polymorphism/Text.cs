433ed6211da8c0fdfbadf02806c3ae5615903114
NanoCode012 <kevinvong@rocketmail.com>
2018-02-23 10:17:32 +0700

    Added concept of Sealed

---
+++ CSharpIntermediate_Polymorphism/Text.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpIntermediate_Polymorphism
+{
+    sealed class Text : Shape //making this sealed means that no classes can derive from it
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("I'm drawing a text");
+        }
+    }
+}
