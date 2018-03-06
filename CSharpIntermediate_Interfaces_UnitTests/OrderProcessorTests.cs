a292f2b792591e7d2fb57939e76f679056209648
NanoCode012 <kevinvong@rocketmail.com>
2018-03-06 11:52:37 +0700

    Added UnitTesting using NUnit
    Interfaces helps to do unit test because we can create a fake concrete class to test a certain portion of a method. See FakeShippingCalculator.

---
+++ CSharpIntermediate_Interfaces_UnitTests/OrderProcessorTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using CSharpIntermediate_Interfaces;
+
+namespace CSharpIntermediate_Interfaces_UnitTests
+{
+    //Name of class follow convention : (CLASSNAME)TESTS
+    [TestFixture()]
+    public class OrderProcessorTests
+    {
+        //Name of method follow convention : (METHODNAME)_CONDITION_EXPECTATIONS
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Process_OrderIsShipped_ThrowsException()
+        {
+            var orderProcessor = new OrderProcessor(new FakeShippingCalculator());
+            var order = new Order { Shipment = new Shipment() };
+            orderProcessor.Process(order);
+        }
+
+        [Test()]
+        public void Process_OrderIsNotShipped_ShouldSetShipmentPropertyOfOrder()
+        {
+            var orderProcessor = new OrderProcessor(new FakeShippingCalculator());
+            var order = new Order();
+            orderProcessor.Process(order);
+            Assert.IsTrue(order.IsShipped);
+            Assert.IsNotNull(order.Shipment);
+            Assert.AreEqual(1, order.Shipment.Cost);
+            Assert.AreEqual(DateTime.Today.AddDays(1), order.Shipment.ShippingDate);
+        }
+    }
+
+    public class FakeShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            return 1f;
+        }
+    }
+}
